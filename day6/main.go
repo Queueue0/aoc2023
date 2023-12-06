6877ce338fe1eaae950b90be7ccb25d4ad7e47eb
Queueue0 <jcoffey97@gmail.com>
2023-12-06 09:46:01 -0500

    Part 6 solution

---
+++ day6/main.go
@@ -0,0 +1,135 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Race struct {
+	MaxTime int
+	MinDist int
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Missing input file arg")
+		os.Exit(1)
+	}
+
+	lines, err := readLines(os.Args[1])
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	// Part 1
+	races := parseInput(lines)
+
+	total := 1
+	for _, race := range races {
+		half := race.MaxTime / 2
+		n := half / 2
+		mini := 0
+		maxi := half
+		fmt.Println(race, half, n)
+
+		for !race.IsFirst(n) {
+			if race.GetDist(n) > race.MinDist {
+				maxi = n - 1
+			} else {
+				mini = n + 1
+			}
+			n = (mini + maxi) / 2
+			fmt.Println(race, half, n)
+		}
+
+		ways := (half - n + 1) * 2
+		if race.MaxTime%2 == 0 {
+			ways--
+		}
+
+		total *= ways
+	}
+
+	fmt.Println(total)
+
+	// Part 2
+	race := parseInput2(lines)
+
+	half := race.MaxTime / 2
+	n := half / 2
+	mini := 0
+	maxi := half
+	fmt.Println(race, half, n)
+
+	for !race.IsFirst(n) {
+		if race.GetDist(n) > race.MinDist {
+			maxi = n - 1
+		} else {
+			mini = n + 1
+		}
+		n = (mini + maxi) / 2
+		fmt.Println(race, half, n)
+	}
+
+	ways := (half - n + 1) * 2
+	if race.MaxTime%2 == 0 {
+		ways--
+	}
+
+	fmt.Println(ways)
+}
+
+func readLines(path string) ([]string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines, scanner.Err()
+}
+
+func parseInput(lines []string) []Race {
+	races := []Race{}
+
+	times := strings.Fields(strings.Split(lines[0], ":")[1])
+	distances := strings.Fields(strings.Split(lines[1], ":")[1])
+
+	for i := range times {
+		time, _ := strconv.Atoi(times[i])
+		dist, _ := strconv.Atoi(distances[i])
+
+		races = append(races, Race{MaxTime: time, MinDist: dist})
+	}
+
+	return races
+}
+
+func parseInput2(lines []string) Race {
+	time := strings.Join(strings.Fields(strings.Split(lines[0], ":")[1]), "")
+	distance := strings.Join(strings.Fields(strings.Split(lines[1], ":")[1]), "")
+
+	t, _ := strconv.Atoi(time)
+	d, _ := strconv.Atoi(distance)
+
+	return Race{MaxTime: t, MinDist: d}
+}
+
+func (r *Race) GetDist(n int) int {
+	return (r.MaxTime - n) * n
+}
+
+func (r *Race) IsFirst(n int) bool {
+	// If n beats the distance and n-1 doesn't
+	return (r.GetDist(n) > r.MinDist && r.GetDist(n-1) <= r.MinDist)
+}
